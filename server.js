15be40302b1142de6886eab3ce4d3201484925f1
Sattar Amrahov <settar216@gmail.com>
2017-10-29 09:27:37 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    
+    fs.appendFile("server.log", log + "\n");
+    next();
+});
+
+
+app.use((req, res, next) => {
+   res.render("maintenance.hbs"); 
+});
+
+
+app.use(express.static(__dirname + "/public"));
+
+
+hbs.registerHelper("getYear", () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", (text) => {
+    return text.toUpperCase();
+});
+
+
+app.get("/", (req, res) => {
+    res.render("home.hbs", {
+        pageTitle: "Home Page",
+        welcomeMessage: "welcome to our website",
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get("/about", (req, res)=>{
+    res.render("about.hbs", {
+        pageTitle: "About",
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get("/bad", (req, res)=>{
+    res.send({
+        errorMessage: "Unable to show PAGE"
+    });
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP, () => {
+    console.log("Server working");
+});
